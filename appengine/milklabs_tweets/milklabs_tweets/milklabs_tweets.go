452ca3fd052780163de7a3391f9513e80ee02b0e
Harnesser <harnesser+lt@gmail.com>
2011-10-06 18:14:34 +0100

    Reading a JSON file and printing it

--- appengine/milklabs_tweets/milklabs_tweets/milklabs_tweets.go
+++ appengine/milklabs_tweets/milklabs_tweets/milklabs_tweets.go
@@ -3,12 +3,32 @@ package milklabs_tweets
 import (
     "fmt"
     "http"
+    "io/ioutil"
 )
 
 func init() {
     http.HandleFunc("/", handler )
+    http.HandleFunc("/marty/", marty_handler )
 }
 
 func handler(w http.ResponseWriter, r *http.Request ) {
     fmt.Fprintf(w, "Hello World! ADSFadsfasdfaadsfadf")
+}
+
+func marty_handler(w http.ResponseWriter, r *http.Request ) {
+    fmt.Fprintf(w, "MARTYTY!!!")
+    read_dev_json(w)
+}
+
+
+func read_dev_json(w http.ResponseWriter){
+
+    // Open file
+    buf, err := ioutil.ReadFile("./dev_data/search.milklabs.json")
+    if err == nil {
+        fmt.Fprintf(w, "%s", buf)
+    } else {
+        fmt.Fprintf(w, "ERERER")
+    }
+    
 }
